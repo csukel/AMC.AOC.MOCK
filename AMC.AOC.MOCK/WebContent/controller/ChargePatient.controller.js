2b6eafce0d6ba5a220a452b63191190cfbe99ab1
l.stylianou <loukkis91@gmail.com>
2016-06-26 22:59:07 +0300

    ChangePatient view was added.
    Navigation to ChangePatient was added.

---
+++ AMC.AOC.MOCK/WebContent/controller/ChargePatient.controller.js
@@ -0,0 +1,49 @@
+sap.ui.define(
+		["amc/aoc/controller/BaseController",
+		 "amc/aoc/model/formatter",
+		 "sap/ui/model/Filter",
+		 "sap/ui/model/FilterOperator",
+		 "sap/ui/core/routing/History"],
+		 function(BaseController,formatter,Filter,FilterOperator,History){
+			"use strict";
+
+			return BaseController.extend("amc.aoc.controller.ChargePatient",{
+
+
+				onInit: function () {
+					var oRouter = this.getRouter();
+					oRouter.getRoute("ChargePatient").attachPatternMatched(this._onObjectMatched, this);
+				},
+				_onObjectMatched: function (oEvent) {
+					this.getView().bindElement({
+						path: "/" + decodeURIComponent(oEvent.getParameter("arguments").patientIndex),
+						model: "patients"
+					});
+				},
+
+				onNavBack : function(){
+
+					var oHistory = History.getInstance(),
+					sPreviousHash = oHistory.getPreviousHash();
+
+					var component = this.getOwnerComponent();
+					var oView = component.byId("homepage");
+					var oIconTabBar = oView.byId("idIconTabBarNoIcons");
+					//oIconTabBar.setSelectedKey("__component0---homepage--tab-__component0---homepage--idIconTabBarNoIcons--header-0");
+					oIconTabBar.setSelectedKey("All");
+					//var homepageController = component.getHomepageController();
+
+					//homepageController.setSelectedTab();
+					if (sPreviousHash !== undefined){
+						// the history contains a previous entry
+						history.go(-1);
+					} else {
+						//otherwise we go backwards with a forward history
+						var bReplace = true;
+						this.getRouter().navTo("homepage",{},bReplace);
+					}
+
+				}
+
+			});
+		});
--- AMC.AOC.MOCK/WebContent/controller/Patients.controller.js
+++ AMC.AOC.MOCK/WebContent/controller/Patients.controller.js
@@ -3,22 +3,33 @@ sap.ui.define(
 		 "amc/aoc/model/formatter",
 		 "sap/ui/model/Filter",
 		 "sap/ui/model/FilterOperator"],
-		function(BaseController,formatter,Filter,FilterOperator){
+		 function(BaseController,formatter,Filter,FilterOperator){
 			"use strict";
-			
+			var oRouter = null;
 			return BaseController.extend("amc.aoc.controller.Patients",{
 				
-				formatter: formatter,
 				
+				formatter: formatter,
+				onInit : function (){
+					jQuery.sap.log.info("Patients","onInit","Patients controller");
+				},
+				
+				onAfterRendering : function(){
+					jQuery.sap.log.info("Patients","onAfterRendering","Patients controller");
+					oRouter = this.getRouter();
+				},
+				onBeforeRendering: function(){
+					jQuery.sap.log.info("Patients","onBeforeRendering","Patients controller");
+				},
 				onSearch: function (oEvent){
-					
+
 					var sQueryString = oEvent.getParameter("query");
 					//var oComboBox = this.getView().byId("__component0---homepage--cbSearchCriteria-__component0---homepage--idIconTabBarNoIcons--header-0");
 					//var oComboBox = this.getView().byId("cbSearchCriteria");
 					//var sCriteria = oComboBox.getSelectedKey();
-					
+
 					var oFilters = [];
-					
+
 					if(sQueryString && sQueryString.length>0) {
 						//var oFilter = new Filter(sCriteria,FilterOperator.Contains,sQueryString);
 						var oNameFilter = new Filter("Name",FilterOperator.Contains,sQueryString);
@@ -32,19 +43,26 @@ sap.ui.define(
 					//var oTileContainer = this.getView().byId("__component0---homepage--tileContainer-__component0---homepage--idIconTabBarNoIcons--header-0");
 					var oTileContainer = this.getView().byId("tileContainer");
 					var oBinding = oTileContainer.getBinding("tiles");
-					
+
 					var aFilters = new Filter(oFilters,false);
-					
+
 					if (oFilters.length === 0){
 						oBinding.filter(oFilters);
 					}else {
 						oBinding.filter(aFilters);
 					}
-					
+
 				},
 				onPatientTilePress: function(oEvent){
 					var oSource = oEvent.getSource();
-				}
-				
+					var sPath = oSource.getBindingContext("patients").getPath();
+					//navigate to Charge Patient page
+					
+					oRouter.navTo("ChargePatient",{
+						patientIndex: encodeURIComponent(sPath.substring(1))
+					}); 
+				},
+
+
 			});
 		});
